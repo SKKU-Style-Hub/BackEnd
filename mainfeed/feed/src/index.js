780dc2a340dbfe44d9b6cf1ca9f498b2bda93ff1
shlee7131 <shlee7131@gmail.com>
2021-04-02 11:29:34 +0900

    limit 10mb

--- mainfeed/feed/src/index.js
+++ mainfeed/feed/src/index.js
@@ -28,7 +28,9 @@ ConsumeStylingPost()
 // ConsumeStylingResponse()
 ConsumeComment()
 
-app.use(express.json())
+app.use(express.json({
+    limit: "10mb"
+}))
 app.use(express.urlencoded({ extended: false }))
 
 app.use(UpdateMainFeedList)

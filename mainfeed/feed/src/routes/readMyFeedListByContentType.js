98f61e6d5f70f416fa64807bd1bcb796d0e6a890
shlee7131 <shlee7131@gmail.com>
2021-04-02 12:25:32 +0900

    feed contenttype list

--- mainfeed/feed/src/routes/readMyFeedListByContentType.js
+++ mainfeed/feed/src/routes/readMyFeedListByContentType.js
@@ -7,7 +7,7 @@ router.post('/api/mainfeed/list/read/content', async (req, res) => {
     const { userProfile, contentType } = req.body
     // 현재 피드 기준 이전 피드 20개 가져오기
     const userNickname = userProfile.userNickname
-    const feedList = await MainFeed.find({ userNickname: userNickname, contentType: contentType }).sort({ "createdAt": 1 })
+    const feedList = await MainFeed.find({ "userProfile.userNickname": userNickname, contentType: contentType }).sort({ "createdAt": 1 })
     // 이후 업데이트
     res.send(feedList)
 })
